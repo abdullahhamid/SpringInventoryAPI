e1f29264ed3a6fd0700e6f17ccea047aa9702305
Abdullah Hamid <abdullahroshan17@gmail.com>
2016-10-31 22:09:44 +0000

    cleanup

--- src/main/java/inventoryapi/ItemController.java
+++ src/main/java/inventoryapi/ItemController.java
@@ -1,26 +1,21 @@
 package inventoryapi;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     private static final Logger log = LoggerFactory.getLogger(Application.class);
     
     @Autowired
@@ -28,17 +23,10 @@ public class ItemController {
 
     
     //GET All Items
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method=RequestMethod.GET)
     public ArrayList<Item> getAllItems() {
     	ArrayList<Item> items = (ArrayList<Item>) repo.findAll();
         return items;
-    }
-
-    
-    @RequestMapping(value = "/items", method=RequestMethod.GET, headers="Accept=application/json")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
     }
     
     //CREATE Item
--- src/test/java/inventoryapi/ApplicationTest.java
+++ src/test/java/inventoryapi/ApplicationTest.java
@@ -5,6 +5,8 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,15 +17,21 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApplicationTest {
+	
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	@Autowired
     private MockMvc mockMvc;
@@ -34,12 +42,6 @@ public class ApplicationTest {
 	@Autowired
     private ItemController controller;
 	
-
-    @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/items")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")));
-    }
     
     @Test
     public void shouldReturnItems() throws Exception {
@@ -50,14 +52,21 @@ public class ApplicationTest {
     
     @Test
     public void shouldReturnCreatedItemObject() throws Exception {
-    	Item item = new Item (1, "kitkat", 5);
+    	Item item = new Item (3, "bounty", 78);
         this.mockMvc.perform(post("/item")
         		.contentType(TestUtil.APPLICATION_JSON_UTF8)
         		.content(TestUtil.convertObjectToJsonBytes(item)))
         		.andDo(print()).andExpect(status().isCreated())
                 .andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.name", containsString("kitkat")))
-                .andExpect(jsonPath("$.id").value(1));
+                .andExpect(jsonPath("$.name", containsString("bounty")))
+                .andExpect(jsonPath("$.id").value(item.getId()));
+        
+        //Delete this item after assertion
+        try{
+    		repo.delete(3);
+    	}catch (Exception e){
+    		log.error(e.getMessage());
+    	}
     }
     
     @Test
@@ -71,15 +80,56 @@ public class ApplicationTest {
                 .andExpect(jsonPath("$.id").value(id));
     }
     
+    @Test
+    public void shouldReturnNotFound() throws Exception {
+    	int id = 3;
+        this.mockMvc.perform(get("/item/"+id)
+        		.contentType(TestUtil.APPLICATION_JSON_UTF8))
+        		.andDo(print()).andExpect(status().isNotFound());
+    }
+    
+    @Test
+    public void shouldReturnItemUpdatedById() throws Exception {
+    	int id = 1;
+    	Item item = new Item (1, "kitkatOrange", 25);
+        this.mockMvc.perform(put("/item/"+id)
+        		.contentType(TestUtil.APPLICATION_JSON_UTF8)
+        		.content(TestUtil.convertObjectToJsonBytes(item)))
+        		.andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.name", containsString(item.getName())))
+                .andExpect(jsonPath("$.quantity").value(item.getQuantity()))
+                .andExpect(jsonPath("$.id").value(id));
+    }
+    
+    @Test
+    public void shouldDeleteItemById() throws Exception {
+    	Item item = new Item (4, "dairymilk", 78);
+    	
+    	//Insert new item in db for this test
+    	try{
+    		repo.save(item);
+    	}catch (Exception e){
+    		log.error(e.getMessage());
+    	}
+    	
+        this.mockMvc.perform(delete("/item/"+item.getId())
+        		.contentType(TestUtil.APPLICATION_JSON_UTF8))
+        		.andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Item id: "+ item.getId() +" deleted!")));
+    }
+    
     @Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 		
+		//populate database
 		Item item1 = new Item(1, "kitkat", 50);
 		Item item2 = new Item(2, "mars", 65);
 		repo.save(item1);
 		repo.save(item2);
 	}
+    
 
 }
